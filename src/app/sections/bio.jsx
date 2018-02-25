033f641a1490eb97b6bfbd1ce4520ddceef12439
JK <kolb0401@gmail.com>
2018-02-24 21:46:40 -0500

    Fixing package versions and changing the background photo

--- src/app/sections/bio.jsx
+++ src/app/sections/bio.jsx
@@ -8,7 +8,7 @@ export default class Bio extends React.Component {
         <div className="container">
             <h2>About Me</h2>
             <p>
-              I am a tech lead / senior engineer for Confirm.io (acquired by Facebook). In my free time I am the volunteer IT Manager
+              I am a senior engineer for Teikametrics. I was perviously at Confirm.io (acquired by Facebook). In my free time I am the volunteer IT Manager
               of <a href="http://rufftalesrescue.org">Ruff Tales Rescue</a>, a 501c3 dog
               rescue organization. I love to experiment with new technologies,
               both frontend and backend, on my personal site. When I am not nerding out
